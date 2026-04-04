b31e1d23ec385f50da11c63f46f7b6f04bdd7745
nothing010101 <mstrj719@gmail.com>
2026-04-04 12:31:44 +0700

    Replace frame-sdk import with miniapp-sdk

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useAccount, useConnect } from 'wagmi'
-import sdk from '@farcaster/frame-sdk'
+import { sdk } from '@farcaster/miniapp-sdk'
 import { usePlayerStore } from '@/store/playerStore'
 import dynamic from 'next/dynamic'
 
