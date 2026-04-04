4280ab5e8ca0296dbd1529c4e6e07c20f20a00e3
nothing010101 <mstrj719@gmail.com>
2026-04-04 14:04:36 +0700

    Update reownConfig.ts

--- config/reownConfig.ts
+++ config/reownConfig.ts
@@ -1,11 +1,12 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { base } from '@reown/appkit/networks'
+import type { AppKitNetwork } from '@reown/appkit/networks'
 
 export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID
 if (!projectId) throw new Error('NEXT_PUBLIC_REOWN_PROJECT_ID is not defined')
 
-export const networks = [base] as const
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [base]
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({ storage: cookieStorage }),
